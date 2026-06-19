48cf5b0e53f6ddf7aca4f9ebfa173a9ae0734ac0
Agustin Marquez <agu.marquez10@gmail.com>
2026-06-19 17:14:35 +0000

    reto 3

--- Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/python/agumarquez10.py
+++ Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/python/agumarquez10.py
@@ -1,17 +1,25 @@
-/*
- * Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
- * Podrás configurar generar contraseñas con los siguientes parámetros:
- * - Longitud: Entre 8 y 16.
- * - Con o sin letras mayúsculas.
- * - Con o sin números.
- * - Con o sin símbolos.
- * (Pudiendo combinar todos estos parámetros entre ellos)
- */
+##Escribe un programa que sea capaz de generar contraseñas de forma aleatoria.
+##Podrás configurar generar contraseñas con los siguientes parámetros:
+##Longitud: Entre 8 y 16.
+##Con o sin letras mayúsculas.
+##Con o sin números.
+##Con o sin símbolos.
+##(Pudiendo combinar todos estos parámetros entre ellos)
+ 
+import random
 
 def randompassword(length=8, capital=False, numbers=False, symbols=False):
+    
+    characters = ["a", "b", "c", "d"]
 
+    password = ""
 
-    return ""
+    final_lenght = 8 if length < 8 else 16 if length > 16 else length
 
-print(randompassword())
+    while len(password) < final_lenght:
+        password += random.choice(characters)
+
+    return password
+
+print(randompassword(length=25))
 
