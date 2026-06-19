9e86b60c61bf265fbc51d4fc7537782176b2b158
Agustin Marquez <agu.marquez10@gmail.com>
2026-06-19 17:32:28 +0000

    reto 3

--- Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/python/agumarquez10.py
+++ Retos/Reto #3 - EL GENERADOR DE CONTRASEÑAS [Media]/python/agumarquez10.py
@@ -10,16 +10,30 @@ import random
 
 def randompassword(length=8, capital=False, numbers=False, symbols=False):
     
-    characters = ["a", "b", "c", "d"]
+    characters = list(range(97, 123))
+
+    if capital:
+        characters += list(range(65, 91))
+
+    if numbers:
+        characters += list(range(49, 58))
+
+    if symbols:
+        characters += list(range(33, 48)) + list(range(58,65)) + list(range(91, 97))
 
     password = ""
 
     final_lenght = 8 if length < 8 else 16 if length > 16 else length
 
     while len(password) < final_lenght:
-        password += random.choice(characters)
+        password += chr(random.choice(characters))
 
     return password
 
-print(randompassword(length=25))
-
+print(randompassword())
+print(randompassword(length=16))
+print(randompassword(length=1))
+print(randompassword(length=22))
+print(randompassword(length=12, capital=True))
+print(randompassword(length=12, capital=True, numbers=True))
+print(randompassword(length=12, capital=True, numbers=True, symbols=True))
